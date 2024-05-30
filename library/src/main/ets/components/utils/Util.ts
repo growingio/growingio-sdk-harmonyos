d5a5130a7fb4b1b756b4264475c7c4a4cc05e4ce
YoloMao <a83506677@126.com>
2024-05-30 14:25:40 +0800

    feat: compressEnabled config

--- library/src/main/ets/components/utils/Util.ts
+++ library/src/main/ets/components/utils/Util.ts
@@ -1,4 +1,6 @@
 import { ValueType, AttributesType } from '../utils/Constants'
+import buffer from '@ohos.buffer'
+import snappy from 'snappyjs'
 
 export default class Util {
   static mapToObject(map: Map<string, ValueType>): { [key: string]: ValueType } {
@@ -42,5 +44,42 @@ export default class Util {
     let lastByteHex: string = hexString.slice(-2)
     let lastByte: number = parseInt(lastByteHex, 16)
     return new Uint8Array([lastByte])
+  }
+
+  static compress(serialize: string): ArrayBuffer {
+    if (typeof serialize != 'string') {
+      throw new TypeError('[GrowingAnalytics] Argument compressed must be type of string')
+    }
+
+    let buf: ArrayBuffer = buffer.from(serialize, 'utf-8').buffer
+    let compressed = snappy.compress(buf) as ArrayBuffer
+    return compressed
+  }
+
+  static encrypt(serialize: string | ArrayBuffer, time: number): ArrayBuffer {
+    let isString: boolean = false
+    if (typeof serialize == 'string') {
+      isString = true
+    } else if (serialize instanceof ArrayBuffer) {
+      isString = false
+    } else {
+      throw new TypeError('[GrowingAnalytics] Argument encrypted must be type of string or ArrayBuffer')
+    }
+
+    let buf: ArrayBuffer
+    if (isString) {
+      buf = buffer.from(serialize as string, 'utf-8').buffer
+    } else {
+      buf = serialize as ArrayBuffer
+    }
+
+    let hint = Util.getHintFromTime(time)
+    let original = new Uint8Array(buf)
+    let encrypted = new Uint8Array(buf.byteLength)
+    for (let i = 0; i < original.length; i++) {
+      encrypted[i] = original[i] ^ hint[i % hint.length]
+    }
+
+    return buffer.from(encrypted).buffer
   }
 }
