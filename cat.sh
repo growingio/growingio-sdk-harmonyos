2ece795a44253e6eea99243513bfc002f7e1b295
tianhui <vickytian123@gmail.com>
2024-05-30 17:22:30 +0800

    test: webview调试方法

---
+++ cat.sh
@@ -0,0 +1,8 @@
+#1. 打开web debug模式 webview.WebviewController.setWebDebuggingAccess(true)
+#2. chrome 访问 chrome://inspect/#devices，在 Discover network targets 中添加 localhost:9222
+#3. 运行 cat.sh
+#原文链接：https://blog.csdn.net/coooliang/article/details/138001902
+name=$(hdc shell ps -ef | grep com.growingio.example | awk '{print $2}')
+echo "prefix_${name}"
+pid=${name##*_}
+hdc fport tcp:9222 localabstract:webview_devtools_remote_$pid
